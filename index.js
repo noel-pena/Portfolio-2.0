112aca6e3291dcd1dff06570bb858d4c552f29be
Noel Pena <127697218+noel-pena@users.noreply.github.com>
2023-12-21 00:18:07 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
 const d = new Date();
-let year = d.getFullYear();
-console.log(year)
+const year = d.getFullYear();
+const time = d.getHours();
+console.log(time)
 
-document.getElementById("foot").innerHTML = "Noel Pena, Inc | Copyright " + year
+function tellTime(){
+    if(time >= 0 && time < 12){
+        return "Good morning.";
+    }
+    if(time >= 12 && time < 15){
+        return "Good afternoon.";
+    }
+    if(time >= 15 && time < 20){
+        return "Good evening."
+    }
+    if(time >= 20 && time < 24){
+        return "Good night."
+    }
+};
+
+document.getElementById("foot").innerHTML = "Noel Pena, Inc | Copyright " + year;
+
+document.getElementById("greet").innerText = tellTime();
